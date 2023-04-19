da3699fc61e76bf6bb00f5b30642333aa7d28573
Alpha-Hamza <m.hamza.alphasquad@gmail.com>
2023-04-19 16:22:35 +0500

    Starter Next js

---
+++ components/AirTable.js
@@ -0,0 +1,11 @@
+
+
+export default function AirTable() {
+
+  return (
+    <div id="divcontents">
+      <h1 className="text-3xl font-bold text-center mb-2">Next JS Starter Template</h1>
+
+    </div>
+  );
+}
